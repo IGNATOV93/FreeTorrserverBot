f925bed1cc31e0478fa57a8079560733ec6ecbf0
IGNATOV93 <ignatowja@gmail.com>
2025-05-02 00:42:08 +0300

    addMigrations

---
+++ FreeTorrBot/Migrations/20250501214109_addnewflagcreateNewprofile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdTorrBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnewflagcreateNewprofile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FlagNewLoginAndPasswordOtherProfile",
+                table: "TextInputFlag",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlagNewLoginAndPasswordOtherProfile",
+                table: "TextInputFlag");
+        }
+    }
+}
